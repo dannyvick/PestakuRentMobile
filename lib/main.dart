a1b7895a8a7cbd9de6b2a4aeecee8042565d267b
dannyvick <dannyvicky.9657@gmail.com>
2022-12-07 23:24:34 +0700

    First Comit

---
+++ lib/main.dart
@@ -0,0 +1,223 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/pestakurentapp/generatedhomewidget/GeneratedHomeWidget.dart';
+import 'package:flutterapp/pestakurentapp/generateddaftarwidget1/GeneratedDaftarWidget1.dart';
+import 'package:flutterapp/pestakurentapp/generatedverifikasikodewidget/GeneratedVerifikasikodeWidget.dart';
+import 'package:flutterapp/pestakurentapp/generatedmasukwidget1/GeneratedMasukWidget1.dart';
+import 'package:flutterapp/pestakurentapp/generatedberandawidget/GeneratedBerandaWidget.dart';
+import 'package:flutterapp/pestakurentapp/generatedkeranjangwidget/GeneratedKeranjangWidget.dart';
+import 'package:flutterapp/pestakurentapp/generatedcheckoutwidget/GeneratedCheckoutWidget.dart';
+import 'package:flutterapp/pestakurentapp/generatedkofirmasipembayaranwidget/GeneratedKofirmasipembayaranWidget.dart';
+import 'package:flutterapp/pestakurentapp/generatedprofilwidget/GeneratedProfilWidget.dart';
+import 'package:flutterapp/pestakurentapp/generatedberandawidget1/GeneratedBerandaWidget1.dart';
+import 'package:flutterapp/pestakurentapp/generatedperingatanwidget/GeneratedPeringatanWidget.dart';
+import 'package:flutterapp/pestakurentapp/generatedhomewidget1/GeneratedHomeWidget1.dart';
+import 'package:flutterapp/pestakurentapp/generatedhomewidget3/GeneratedHomeWidget3.dart';
+import 'package:flutterapp/pestakurentapp/generatedverifikasikodewidget1/GeneratedVerifikasikodeWidget1.dart';
+import 'package:flutterapp/pestakurentapp/generatedmasukwidget7/GeneratedMasukWidget7.dart';
+import 'package:flutterapp/pestakurentapp/generatedberandawidget2/GeneratedBerandaWidget2.dart';
+import 'package:flutterapp/pestakurentapp/generatedprofilwidget1/GeneratedProfilWidget1.dart';
+import 'package:flutterapp/pestakurentapp/generatedlaporanbarangseewaanwidget/GeneratedLaporanbarangseewaanWidget.dart';
+import 'package:flutterapp/pestakurentapp/generatedhomewidget4/GeneratedHomeWidget4.dart';
+import 'package:flutterapp/pestakurentapp/generatedverifikasikodewidget2/GeneratedVerifikasikodeWidget2.dart';
+import 'package:flutterapp/pestakurentapp/generatedmasukwidget11/GeneratedMasukWidget11.dart';
+import 'package:flutterapp/pestakurentapp/generatedberandawidget3/GeneratedBerandaWidget3.dart';
+import 'package:flutterapp/pestakurentapp/generatedprofilwidget2/GeneratedProfilWidget2.dart';
+import 'package:flutterapp/pestakurentapp/generatedlaporanbarangseewaanwidget1/GeneratedLaporanbarangseewaanWidget1.dart';
+import 'package:flutterapp/pestakurentapp/generatediphone1313pro1widget/GeneratedIPhone1313Pro1Widget.dart';
+import 'package:flutterapp/pestakurentapp/generatedhalaman5widget/GeneratedHalaman5Widget.dart';
+import 'package:flutterapp/pestakurentapp/generatediphone1313pro2widget/GeneratedIPhone1313Pro2Widget.dart';
+import 'package:flutterapp/pestakurentapp/generatedhalaman5widget2/GeneratedHalaman5Widget2.dart';
+import 'package:flutterapp/pestakurentapp/generatedhalaman4widget/GeneratedHalaman4Widget.dart';
+import 'package:flutterapp/pestakurentapp/generatedhalaman3widget/GeneratedHalaman3Widget.dart';
+import 'package:flutterapp/pestakurentapp/generatedhalaman1widget/GeneratedHalaman1Widget.dart';
+import 'package:flutterapp/pestakurentapp/generatedhalamanloginwidget/GeneratedHalamanLoginWidget.dart';
+import 'package:flutterapp/pestakurentapp/generatedlupapasswordwidget1/GeneratedLupaPasswordWidget1.dart';
+import 'package:flutterapp/pestakurentapp/generatedhalamankodeverifikasiwidget/GeneratedHalamanKodeVerifikasiWidget.dart';
+import 'package:flutterapp/pestakurentapp/generatedkonfirmasipasswordbaruwidget/GeneratedKonfirmasiPasswordBaruWidget.dart';
+import 'package:flutterapp/pestakurentapp/generatedhalamankodeverifikasiwidget1/GeneratedHalamanKodeVerifikasiWidget1.dart';
+import 'package:flutterapp/pestakurentapp/generatediphone1313pro5widget/GeneratedIPhone1313Pro5Widget.dart';
+import 'package:flutterapp/pestakurentapp/generatediphone1313pro1widget2/GeneratedIPhone1313Pro1Widget2.dart';
+import 'package:flutterapp/pestakurentapp/generatedformdaftarwidget1/GeneratedFormdaftarWidget1.dart';
+import 'package:flutterapp/pestakurentapp/generatedkodeverifikasiwidget/GeneratedKodeverifikasiWidget.dart';
+import 'package:flutterapp/pestakurentapp/generatedmasukwidget19/GeneratedMasukWidget19.dart';
+import 'package:flutterapp/pestakurentapp/generatedprofilwidget4/GeneratedProfilWidget4.dart';
+import 'package:flutterapp/pestakurentapp/generatedtextwidget/GeneratedTextWidget.dart';
+import 'package:flutterapp/pestakurentapp/generatedberandawidget4/GeneratedBerandaWidget4.dart';
+import 'package:flutterapp/pestakurentapp/generatedcheckoutwidget3/GeneratedCheckoutWidget3.dart';
+import 'package:flutterapp/pestakurentapp/generatedperingatanwidget1/GeneratedPeringatanWidget1.dart';
+import 'package:flutterapp/pestakurentapp/generatedkeranjangwidget1/GeneratedKeranjangWidget1.dart';
+import 'package:flutterapp/pestakurentapp/generatedcheckoutwidget4/GeneratedCheckoutWidget4.dart';
+import 'package:flutterapp/pestakurentapp/generatedkonfirmsipembayaranwidget/GeneratedKonfirmsipembayaranWidget.dart';
+import 'package:flutterapp/pestakurentapp/generateddaftarwidget11/GeneratedDaftarWidget11.dart';
+import 'package:flutterapp/pestakurentapp/generatedmasukwidget21/GeneratedMasukWidget21.dart';
+import 'package:flutterapp/pestakurentapp/generatedframe1widget/GeneratedFrame1Widget.dart';
+import 'package:flutterapp/pestakurentapp/generatedverifikasikodewidget3/GeneratedVerifikasikodeWidget3.dart';
+import 'package:flutterapp/pestakurentapp/generatedberandawidget5/GeneratedBerandaWidget5.dart';
+import 'package:flutterapp/pestakurentapp/generateddesktop7widget/GeneratedDesktop7Widget.dart';
+import 'package:flutterapp/pestakurentapp/generatedcolumn0widget/GeneratedColumn0Widget.dart';
+import 'package:flutterapp/pestakurentapp/generatedcolumn0widget1/GeneratedColumn0Widget1.dart';
+import 'package:flutterapp/pestakurentapp/generatedcolumn0widget2/GeneratedColumn0Widget2.dart';
+import 'package:flutterapp/pestakurentapp/generatedcolumn0widget3/GeneratedColumn0Widget3.dart';
+import 'package:flutterapp/pestakurentapp/generatedcolumn0widget4/GeneratedColumn0Widget4.dart';
+import 'package:flutterapp/pestakurentapp/generatedcolumn0widget5/GeneratedColumn0Widget5.dart';
+import 'package:flutterapp/pestakurentapp/generatedberandawidget6/GeneratedBerandaWidget6.dart';
+import 'package:flutterapp/pestakurentapp/generatedmasukwidget24/GeneratedMasukWidget24.dart';
+import 'package:flutterapp/pestakurentapp/generatedprofilwidget6/GeneratedProfilWidget6.dart';
+import 'package:flutterapp/pestakurentapp/generatedverifikasikodewidget5/GeneratedVerifikasikodeWidget5.dart';
+import 'package:flutterapp/pestakurentapp/generatedlaporansewawidget/GeneratedLaporansewaWidget.dart';
+import 'package:flutterapp/pestakurentapp/generatedperingatanwidget2/GeneratedPeringatanWidget2.dart';
+import 'package:flutterapp/pestakurentapp/generatedberandawidget7/GeneratedBerandaWidget7.dart';
+import 'package:flutterapp/pestakurentapp/generatedkeranjangwidget4/GeneratedKeranjangWidget4.dart';
+import 'package:flutterapp/pestakurentapp/generatedmasukwidget29/GeneratedMasukWidget29.dart';
+import 'package:flutterapp/pestakurentapp/generatedverifikasikodewidget7/GeneratedVerifikasikodeWidget7.dart';
+import 'package:flutterapp/pestakurentapp/generatedprofilwidget8/GeneratedProfilWidget8.dart';
+import 'package:flutterapp/pestakurentapp/generatedberandawidget8/GeneratedBerandaWidget8.dart';
+import 'package:flutterapp/pestakurentapp/generatedkeranjangwidget5/GeneratedKeranjangWidget5.dart';
+import 'package:flutterapp/pestakurentapp/generatedkonfirmsipembayaranwidget1/GeneratedKonfirmsipembayaranWidget1.dart';
+import 'package:flutterapp/pestakurentapp/generateddaftarwidget22/GeneratedDaftarWidget22.dart';
+import 'package:flutterapp/pestakurentapp/generateddekor7widget7/GeneratedDekor7Widget7.dart';
+import 'package:flutterapp/pestakurentapp/generatedlaporansewawidget1/GeneratedLaporansewaWidget1.dart';
+import 'package:flutterapp/pestakurentapp/generatedmasukwidget33/GeneratedMasukWidget33.dart';
+import 'package:flutterapp/pestakurentapp/generatedverifikasikodewidget9/GeneratedVerifikasikodeWidget9.dart';
+import 'package:flutterapp/pestakurentapp/generatedberandawidget9/GeneratedBerandaWidget9.dart';
+import 'package:flutterapp/pestakurentapp/generatedprofilwidget10/GeneratedProfilWidget10.dart';
+import 'package:flutterapp/pestakurentapp/generatedcellwidget20/GeneratedCellWidget20.dart';
+import 'package:flutterapp/pestakurentapp/generatedrowwidget4/GeneratedRowWidget4.dart';
+import 'package:flutterapp/pestakurentapp/generatedtable1widget1/GeneratedTable1Widget1.dart';
+import 'package:flutterapp/pestakurentapp/generatedtooltipwidget/GeneratedTooltipWidget.dart';
+import 'package:flutterapp/pestakurentapp/generatedtooltipwidget1/GeneratedTooltipWidget1.dart';
+import 'package:flutterapp/pestakurentapp/generatedtooltipwidget2/GeneratedTooltipWidget2.dart';
+
+void main() {
+  runApp(PestakuRentApp());
+}
+
+class PestakuRentApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedHomeWidget',
+      routes: {
+        '/GeneratedHomeWidget': (context) => GeneratedHomeWidget(),
+        '/GeneratedDaftarWidget1': (context) => GeneratedDaftarWidget1(),
+        '/GeneratedVerifikasikodeWidget': (context) =>
+            GeneratedVerifikasikodeWidget(),
+        '/GeneratedMasukWidget1': (context) => GeneratedMasukWidget1(),
+        '/GeneratedBerandaWidget': (context) => GeneratedBerandaWidget(),
+        '/GeneratedKeranjangWidget': (context) => GeneratedKeranjangWidget(),
+        '/GeneratedCheckoutWidget': (context) => GeneratedCheckoutWidget(),
+        '/GeneratedKofirmasipembayaranWidget': (context) =>
+            GeneratedKofirmasipembayaranWidget(),
+        '/GeneratedProfilWidget': (context) => GeneratedProfilWidget(),
+        '/GeneratedBerandaWidget1': (context) => GeneratedBerandaWidget1(),
+        '/GeneratedPeringatanWidget': (context) => GeneratedPeringatanWidget(),
+        '/GeneratedHomeWidget1': (context) => GeneratedHomeWidget1(),
+        '/GeneratedHomeWidget3': (context) => GeneratedHomeWidget3(),
+        '/GeneratedVerifikasikodeWidget1': (context) =>
+            GeneratedVerifikasikodeWidget1(),
+        '/GeneratedMasukWidget7': (context) => GeneratedMasukWidget7(),
+        '/GeneratedBerandaWidget2': (context) => GeneratedBerandaWidget2(),
+        '/GeneratedProfilWidget1': (context) => GeneratedProfilWidget1(),
+        '/GeneratedLaporanbarangseewaanWidget': (context) =>
+            GeneratedLaporanbarangseewaanWidget(),
+        '/GeneratedHomeWidget4': (context) => GeneratedHomeWidget4(),
+        '/GeneratedVerifikasikodeWidget2': (context) =>
+            GeneratedVerifikasikodeWidget2(),
+        '/GeneratedMasukWidget11': (context) => GeneratedMasukWidget11(),
+        '/GeneratedBerandaWidget3': (context) => GeneratedBerandaWidget3(),
+        '/GeneratedProfilWidget2': (context) => GeneratedProfilWidget2(),
+        '/GeneratedLaporanbarangseewaanWidget1': (context) =>
+            GeneratedLaporanbarangseewaanWidget1(),
+        '/GeneratedIPhone1313Pro1Widget': (context) =>
+            GeneratedIPhone1313Pro1Widget(),
+        '/GeneratedHalaman5Widget': (context) => GeneratedHalaman5Widget(),
+        '/GeneratedIPhone1313Pro2Widget': (context) =>
+            GeneratedIPhone1313Pro2Widget(),
+        '/GeneratedHalaman5Widget2': (context) => GeneratedHalaman5Widget2(),
+        '/GeneratedHalaman4Widget': (context) => GeneratedHalaman4Widget(),
+        '/GeneratedHalaman3Widget': (context) => GeneratedHalaman3Widget(),
+        '/GeneratedHalaman1Widget': (context) => GeneratedHalaman1Widget(),
+        '/GeneratedHalamanLoginWidget': (context) =>
+            GeneratedHalamanLoginWidget(),
+        '/GeneratedLupaPasswordWidget1': (context) =>
+            GeneratedLupaPasswordWidget1(),
+        '/GeneratedHalamanKodeVerifikasiWidget': (context) =>
+            GeneratedHalamanKodeVerifikasiWidget(),
+        '/GeneratedKonfirmasiPasswordBaruWidget': (context) =>
+            GeneratedKonfirmasiPasswordBaruWidget(),
+        '/GeneratedHalamanKodeVerifikasiWidget1': (context) =>
+            GeneratedHalamanKodeVerifikasiWidget1(),
+        '/GeneratedIPhone1313Pro5Widget': (context) =>
+            GeneratedIPhone1313Pro5Widget(),
+        '/GeneratedIPhone1313Pro1Widget2': (context) =>
+            GeneratedIPhone1313Pro1Widget2(),
+        '/GeneratedFormdaftarWidget1': (context) =>
+            GeneratedFormdaftarWidget1(),
+        '/GeneratedKodeverifikasiWidget': (context) =>
+            GeneratedKodeverifikasiWidget(),
+        '/GeneratedMasukWidget19': (context) => GeneratedMasukWidget19(),
+        '/GeneratedProfilWidget4': (context) => GeneratedProfilWidget4(),
+        '/GeneratedTextWidget': (context) => GeneratedTextWidget(),
+        '/GeneratedBerandaWidget4': (context) => GeneratedBerandaWidget4(),
+        '/GeneratedCheckoutWidget3': (context) => GeneratedCheckoutWidget3(),
+        '/GeneratedPeringatanWidget1': (context) =>
+            GeneratedPeringatanWidget1(),
+        '/GeneratedKeranjangWidget1': (context) => GeneratedKeranjangWidget1(),
+        '/GeneratedCheckoutWidget4': (context) => GeneratedCheckoutWidget4(),
+        '/GeneratedKonfirmsipembayaranWidget': (context) =>
+            GeneratedKonfirmsipembayaranWidget(),
+        '/GeneratedDaftarWidget11': (context) => GeneratedDaftarWidget11(),
+        '/GeneratedMasukWidget21': (context) => GeneratedMasukWidget21(),
+        '/GeneratedFrame1Widget': (context) => GeneratedFrame1Widget(),
+        '/GeneratedVerifikasikodeWidget3': (context) =>
+            GeneratedVerifikasikodeWidget3(),
+        '/GeneratedBerandaWidget5': (context) => GeneratedBerandaWidget5(),
+        '/GeneratedDesktop7Widget': (context) => GeneratedDesktop7Widget(),
+        '/GeneratedColumn0Widget': (context) => GeneratedColumn0Widget(),
+        '/GeneratedColumn0Widget1': (context) => GeneratedColumn0Widget1(),
+        '/GeneratedColumn0Widget2': (context) => GeneratedColumn0Widget2(),
+        '/GeneratedColumn0Widget3': (context) => GeneratedColumn0Widget3(),
+        '/GeneratedColumn0Widget4': (context) => GeneratedColumn0Widget4(),
+        '/GeneratedColumn0Widget5': (context) => GeneratedColumn0Widget5(),
+        '/GeneratedBerandaWidget6': (context) => GeneratedBerandaWidget6(),
+        '/GeneratedMasukWidget24': (context) => GeneratedMasukWidget24(),
+        '/GeneratedProfilWidget6': (context) => GeneratedProfilWidget6(),
+        '/GeneratedVerifikasikodeWidget5': (context) =>
+            GeneratedVerifikasikodeWidget5(),
+        '/GeneratedLaporansewaWidget': (context) =>
+            GeneratedLaporansewaWidget(),
+        '/GeneratedPeringatanWidget2': (context) =>
+            GeneratedPeringatanWidget2(),
+        '/GeneratedBerandaWidget7': (context) => GeneratedBerandaWidget7(),
+        '/GeneratedKeranjangWidget4': (context) => GeneratedKeranjangWidget4(),
+        '/GeneratedMasukWidget29': (context) => GeneratedMasukWidget29(),
+        '/GeneratedVerifikasikodeWidget7': (context) =>
+            GeneratedVerifikasikodeWidget7(),
+        '/GeneratedProfilWidget8': (context) => GeneratedProfilWidget8(),
+        '/GeneratedBerandaWidget8': (context) => GeneratedBerandaWidget8(),
+        '/GeneratedKeranjangWidget5': (context) => GeneratedKeranjangWidget5(),
+        '/GeneratedKonfirmsipembayaranWidget1': (context) =>
+            GeneratedKonfirmsipembayaranWidget1(),
+        '/GeneratedDaftarWidget22': (context) => GeneratedDaftarWidget22(),
+        '/GeneratedDekor7Widget7': (context) => GeneratedDekor7Widget7(),
+        '/GeneratedLaporansewaWidget1': (context) =>
+            GeneratedLaporansewaWidget1(),
+        '/GeneratedMasukWidget33': (context) => GeneratedMasukWidget33(),
+        '/GeneratedVerifikasikodeWidget9': (context) =>
+            GeneratedVerifikasikodeWidget9(),
+        '/GeneratedBerandaWidget9': (context) => GeneratedBerandaWidget9(),
+        '/GeneratedProfilWidget10': (context) => GeneratedProfilWidget10(),
+        '/GeneratedCellWidget20': (context) => GeneratedCellWidget20(),
+        '/GeneratedRowWidget4': (context) => GeneratedRowWidget4(),
+        '/GeneratedTable1Widget1': (context) => GeneratedTable1Widget1(),
+        '/GeneratedTooltipWidget': (context) => GeneratedTooltipWidget(),
+        '/GeneratedTooltipWidget1': (context) => GeneratedTooltipWidget1(),
+        '/GeneratedTooltipWidget2': (context) => GeneratedTooltipWidget2(),
+      },
+    );
+  }
+}
